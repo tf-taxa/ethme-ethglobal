00cc0e2c4b1d56c29135eab539259fa5611b717b
abdulmoiz251 <abdulmoiz251@gmail.com>
2024-09-21 16:37:26 +0500

    order wise redirection for ens fields

--- js/home.js
+++ js/home.js
@@ -25,10 +25,48 @@ async function initialize() {
     let encoded_content_hash = ens_data.contentHash
     
     
-    let content_hash = decodeContentHashWithLink(encoded_content_hash)
-    console.log('content_hash', content_hash);
+    // 1. if index field present then redirect to index field url
+    if (ar_texts && ar_texts.includes('index')) {
+      await initializeWeb3(false, true)
+
+      let index_field_url = await getIndexRecordForENSName(ens_name_hash, resolver_address, encoded_content_hash)
+      console.log('index_field_url', index_field_url);
+      
+      if (index_field_url && index_field_url != '') {
+        redirect_url = index_field_url + url_path
+      }
+    }
     
-    redirect_url = content_hash + url_path
+    // 2. if index field is not set, redirect to contenthash
+    if(!redirect_url && encoded_content_hash && encoded_content_hash != '' && encoded_content_hash != '0x') {
+      let content_hash = decodeContentHashWithLink(encoded_content_hash)
+      console.log('content_hash', content_hash);
+      
+      if (content_hash && content_hash != '') {
+        redirect_url = content_hash + url_path
+      }
+    }
+    
+    // 3. if index & contenthash fields are not set, redirect to url field
+    if(!redirect_url && ar_texts && ar_texts.includes('url')) {
+      await initializeWeb3(false, true)
+
+      let url = await getURLRecordForENSName(ens_name_hash, resolver_address)
+      console.log('url', url);
+
+      if (url && url != '') {
+        redirect_url = url + url_path
+      }
+    }
+
+    // 4. if index, contenthash & url fields are not set, redirect to ens info page
+    if(!redirect_url) {
+      redirect_url = constants.ens_app_url + ens_name
+    }
+    
+    console.timeEnd('home'); // logs the time taken
+    console.log('redirect_url', redirect_url);
+
     window.location.replace(redirect_url)
   } 
   catch (error) {
